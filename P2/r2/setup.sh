b97ffdcc44830d7e5063eccbd9d2e0fe5e42243d
LXV <lxvsws@gmail.com>
2025-04-18 16:46:54 +0200

    save

--- P2/r2/setup.sh
+++ P2/r2/setup.sh
@@ -5,7 +5,7 @@ ip link add br0 type bridge
 ip link set dev br0 up
 
 # Set up and enable the VXLAN interface (vxlan10)
-ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.1 local 10.1.1.2 dstport 4789
+ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
 ip addr add 20.1.1.2/24 dev vxlan10
 ip link set dev vxlan10 up
 
