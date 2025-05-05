d9e8a5a9a479d50370495b0557f2af771b1bb15a
LXV <lxvsws@gmail.com>
2025-05-05 10:02:55 +0200

    save

---
+++ P3/r4/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set up and enable the bridge interface (br0)
+ip link add br0 type bridge
+ip link set dev br0 up
+
+# Set up and enable the VXLAN interface (vxlan10)
+ip link add vxlan10 type vxlan id 10 dstport 4789
+ip link set dev vxlan10 up
+
+# Add interfaces to the bridge (br0)
+brctl addif br0 vxlan10
+brctl addif br0 eth0
